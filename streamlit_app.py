8045947e9bcbe16b5c9a3a5d036c2c02db1c982f
Huu Huynh Truong <45187247+Huynh-Tr@users.noreply.github.com>
2025-02-09 15:00:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 
 # Add custom CSS to hide the GitHub icon
 hide_github_icon = """
-#MainMenu {visibility: hidden;}
+<style>
+#GithubIcon {visibility: hidden;}
+</style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
