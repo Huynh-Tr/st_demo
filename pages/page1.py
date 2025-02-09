54d42563346576cdc74779334769bfb9b6d8549c
Huu Huynh Truong <45187247+Huynh-Tr@users.noreply.github.com>
2025-02-09 14:25:41 +0700

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
+hide_all_icon = """
+#MainMenu {
   visibility: hidden;
 }
 """
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(hide_all_icon, unsafe_allow_html=True)
 
 st.title("😂")
 
