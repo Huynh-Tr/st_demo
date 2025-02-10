ff80f500e32d3424a81b609c0f1336baa6d80c2f
Huu Huynh Truong <45187247+Huynh-Tr@users.noreply.github.com>
2025-02-10 08:19:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,17 @@ st.markdown(
 )
 
 # Hide sidebar
-st.set_page_config(initial_sidebar_state="collapsed")
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 st.markdown(
     """
