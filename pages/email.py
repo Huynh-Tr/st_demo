9e215179411e6db57b53e005134a1a2d30ec655b
Huu Huynh Truong <45187247+Huynh-Tr@users.noreply.github.com>
2025-02-11 17:00:49 +0700

    Update email.py

--- pages/email.py
+++ pages/email.py
@@ -42,8 +42,8 @@ if not st.session_state.otp_generated: # Only generate if OTP is not generated y
     # np.random.seed(0) # Keep seed for testing purpose. Remove it in production.
     st.session_state.otp = OTP  # Store OTP in session state
     # st.write(OTP)
-st.write('fill out the email')
-send_to_email = st.text_input('')
+
+send_to_email = st.text_input('fill out the email')
 if st.button("Send Email") and not st.session_state.otp_generated:  # Only send if OTP hasn't been sent.
     try:
         yag = yagmail.SMTP(st.secrets["gmail"]["user"], st.secrets["gmail"]["password"])
